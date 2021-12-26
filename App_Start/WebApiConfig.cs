a25cd2affa85c807de4815c49cec1803eb561bb5
Emirhan Kaya <42123355+Emirhan58@users.noreply.github.com>
2021-12-26 13:03:49 +0300

    Add files via upload

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+namespace WebProgrammingProject
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+
+            config.MapHttpAttributeRoutes();
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{api}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //    );
+        }
+    }
+}
